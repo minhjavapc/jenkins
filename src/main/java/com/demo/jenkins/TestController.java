b9e9c6096a24d2b7f0a470299606bf938192f247
minhjavapc <104562096+Mingbae0502@users.noreply.github.com>
2022-08-03 16:00:57 +0700

    first

---
+++ src/main/java/com/demo/jenkins/TestController.java
@@ -0,0 +1,15 @@
+package com.demo.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/")
+    public String test(){
+
+        return "Hello Test";
+    }
+}
